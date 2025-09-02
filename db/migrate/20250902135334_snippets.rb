b93339315e546421febb1eeb66068c9b6adff9a7
Wellington Araújo <wellingtonaraujosoares@gmail.com>
2025-09-02 11:27:37 -0300

    feature: criando tabela snippets no banco de dados

---
+++ db/migrate/20250902135334_snippets.rb
@@ -0,0 +1,10 @@
+class Snippets < ActiveRecord::Migration[8.0]
+  def change
+    create_table :snippets do |t|
+      t.text :text
+      t.text :summary
+      # Add more columns as needed
+      t.timestamps # Adds 'created_at' and 'updated_at' columns
+    end
+  end
+end
