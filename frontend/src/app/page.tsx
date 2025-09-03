779d225e8aaf73d511eeac05bd4b57baa22cd63e
Wellington Araújo <wellingtonaraujosoares@gmail.com>
2025-09-03 20:56:54 -0300

    feature: criando componente InputArea e fazendo cobertura de testes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,23 @@
+"use client"
 
+import InputArea from "@/components/InputArea";
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState<string>("");
+
+  const handleSubmit = () => {
+    console.log(text)
+  }
+
   return (
     <div>
-      <main>
-        <div>
-          <h1>Snippets AI</h1>
-          <span>Simplifique a informação. Amplifique sua produtividade.</span>
+      <main className="flex flex-col w-full items-center px-[30]">
+        <div className="w-full mb-20 mt-15">
+          <h1 className="text-center text-[38px] font-bold">Snippets AI</h1>
+          <p className="text-center w-full text-[20px]">Simplifique a informação. Amplifique sua produtividade.</p>
         </div>
-        <div>
- 
-        </div>
+        <InputArea buttonText="✨ Gerar resumo ✨" value={text} onChange={setText} onClick={handleSubmit}/>
       </main>
       <footer>
       </footer>
---
+++ frontend/src/components/InputArea/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+
+// import { Container } from './styles';
+
+interface IProps {
+    buttonText: string,
+    placeholder?: string,
+    value: string,
+    onChange: (value: string) => void;
+    onClick?: () => void;
+    disabled?: boolean;
+}
+
+const InputArea: React.FC<IProps> = ({ buttonText, onChange, onClick, value, disabled = false, placeholder }) => {
+    return (
+        <div className="flex flex-col w-full max-w-[800] item-center relative">
+            <Textarea placeholder={placeholder} className="mb-5" onChange={(e) => onChange(e.target.value)} value={value} disabled={disabled}/>
+            {value && <Button className="absolute w-50  bottom-10 right-5" size="lg" onClick={onClick} disabled={disabled}>{buttonText}</Button>}
+        </div>
+    )
+}
+
+export default InputArea;
